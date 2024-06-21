d1507112b0f7f691824da7c1d51a1a9c32e2cb4d
x4la1 <lebedevdenis805@gmail.com>
2024-06-21 11:05:58 +0300

    via upload

---
+++ pizza_proj/src/Kernel.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+require_once dirname(__DIR__)."/vendor/autoload_runtime.php";
+
+use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Component\HttpKernel\Kernel as BaseKernel;
+
+class Kernel extends BaseKernel
+{
+    use MicroKernelTrait;
+}
